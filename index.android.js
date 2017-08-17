f6efaac39f590c87050775d9eed1f4977e50ef06
Your Name <1004260403@qq.com>
2017-08-17 15:40:29 +0800

    Justify Content

--- index.android.js
+++ index.android.js
@@ -42,8 +42,13 @@ import {
 class LotsOfStyles extends Component {
     render() {
         return (
-            // 尝试把`flexDirection`改为`column`看看
-            <View style={{flex: 1, flexDirection: 'row'}}>
+            // 尝试把`justifyContent`改为`center`看看
+            // 尝试把`flexDirection`改为`row`看看
+            <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+            }}>
                 <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
                 <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
                 <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
