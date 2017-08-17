55c7ea1cf3d91b1064c273b54a041c83e1ccafa0
Your Name <1004260403@qq.com>
2017-08-17 15:04:17 +0800

    基本样式，风格

--- index.android.js
+++ index.android.js
@@ -13,77 +13,33 @@ import {
     Image,
 } from 'react-native';
 
-export default class Greeting extends Component {
+class LotsOfStyles extends Component {
     render() {
-        let pic = {
-            uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-        };
         return (
-            // 多组件可以用View包裹，单组件则直接显示即可！
             <View>
-                <Text>Hello world！</Text>
-                <Image source={pic} style={{width: 193, height: 110}}/>
-                <Text>Hello {this.props.name}!</Text>
-            </View>
-        );
-    }
-}
-
-/**
- * 自定义的组件也可以使用props,
- * 通过在不同的场景使用不同的属性定制，可以尽量提高自定义组件的复用范畴。
- */
-class LotsOfGreetings extends Component {
-    render() {
-        return (
-            <View style={{alignItems: 'center'}}>
-                <Greeting name='Rexxar'/>
-                <Greeting name='Jaina'/>
-                <Greeting name='Valeera'/>
+                <Text style = {styles.red}>just red</Text>
+                <Text style={styles.bigblue}>just bigblue</Text>
+                <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+                <Text style = {[styles.red, styles.bigblue]}>red, then bigblue</Text>
             </View>
         )
     }
 }
 
-class Blink extends Component {
 
-    // 在constructor中初始化state
-    constructor(props) {
-        super(props);
-        this.state = {showText: true};
+// 定义样式
+// 常见的做法是按顺序声明和使用style属性，
+// 以借鉴CSS中的“层叠”做法（即后声明的属性会覆盖先声明的同名属性）。
+const styles = StyleSheet.create({
 
-        // 每1000毫秒对showText状态做一次取反操作
-        setInterval(() => {
-            this.setState(previousState => {
-                return {showText: !previousState.showText};
-            });
-        }, 1000);
+    bigblue:{
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+    },
+    red:{
+        color:'red',
     }
+})
 
-    render() {
-        // 根据当前showText的值决定是否显示text内容
-        let display = this.state.showText ? this.props.text : '';
-        return (<Text>{display}</Text>)
-    }
-
-}
-
-/**
- * 2、 制作一段不停闪烁的文字
- */
-
-class BlinkApp extends Component {
-    render() {
-        return (
-            <View>
-                <Blink text='I love to blink'/>
-                <Blink text='Yes blinking is so great'/>
-                <Blink text='Why did they ever take this out of HTML'/>
-                <Blink text='Look at me look at me look at me'/>
-            </View>
-        );
-    }
-}
-
-
-AppRegistry.registerComponent('ReactNative_View', () => BlinkApp);
+AppRegistry.registerComponent('ReactNative_View', () => LotsOfStyles);
