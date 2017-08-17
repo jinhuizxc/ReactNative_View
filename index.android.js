278c94bb847a6c00c43b7edb923f6ddd7fdd3fee
Your Name <1004260403@qq.com>
2017-08-17 17:23:36 +0800

    圆圈进度条！

--- index.android.js
+++ index.android.js
@@ -4,70 +4,90 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {
-    AppRegistry,
-    NativeModules,
-    LayoutAnimation,
-    Text,
-    TouchableOpacity,
-    StyleSheet,
-    View,
-    AccessibilityInfo,
-} from 'react-native';
-
 /**
- * 动画
- * React Native提供了两个互补的动画系统：
- * 用于全局的布局动画LayoutAnimation，和用于创建更精细的交互控制的动画Animated。
- * LayoutAnimation
- LayoutAnimation允许你在全局范围内创建和更新动画，这些动画会在下一次渲染或布局周期运行。
- 它常用来更新flexbox布局，因为它可以无需测量或者计算特定属性就能直接产生动画。尤其是当布局变化可能影响到父节点（譬如“查看更多”展开动画既增加父节点的尺寸又会将位于本行之下的所有行向下推动）时，如果不使用LayoutAnimation，可能就需要显式声明组件的坐标，才能使得所有受影响的组件能够同步运行动画。
- 注意尽管LayoutAnimation非常强大且有用，但它对动画本身的控制没有Animated或者其它动画库那样方便，
- 所以如果你使用LayoutAnimation无法实现一个效果，那可能还是要考虑其他的方案。
+ * ActivityIndicator
+ * 显示一个圆形的loading提示符号。
  */
 
-class ScreenReaderStatusExample extends React.Component {
-    state = {
-        screenReaderEnabled: false,
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    ActivityIndicator,
+    TouchableOpacity
+} from 'react-native';
+
+class RNActivityIndicatorDemo extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {// 初始设为显示加载动画
+            animating: true,
+        };
     }
 
-    componentDidMount() {
-        AccessibilityInfo.addEventListener(
-            'change',
-            this._handleScreenReaderToggled
-        );
-        AccessibilityInfo.fetch().done((isEnabled) => {
+    // 按钮响应方法，切换显示与隐藏
+    showOrHide() {
+        if (this.state.animating) {
             this.setState({
-                screenReaderEnabled: isEnabled
+                animating: false
             });
-        });
-    }
-
-    componentWillUnmount() {
-        AccessibilityInfo.removeEventListener(
-            'change',
-            this._handleScreenReaderToggled
-        );
-    }
-
-    _handleScreenReaderToggled = (isEnabled) => {
-        this.setState({
-            screenReaderEnabled: isEnabled,
-        });
+        } else {
+            this.setState({
+                animating: true
+            });
+        }
     }
 
     render() {
         return (
-            <View>
-                <Text>
-                    The screen reader is {this.state.screenReaderEnabled ? 'enabled' : 'disabled'}.
-                </Text>
+            <View style={styles.container}>
+                {/* 切换显示或隐藏的按钮 */}
+                <TouchableOpacity underlayColor="#fff" style={styles.btn} onPress={
+                    this.showOrHide.bind(this)}>
+                    <Text style={{color:'#fff', fontSize: 20}}>显示/隐藏</Text>
+                </TouchableOpacity>
+                {/* 小号的指示器 */}
+                <ActivityIndicator
+                    animating={this.state.animating}
+                    style={[styles.centering, {height: 80}]}
+                    size="small" />
+                {/* 大号的指示器 */}
+                <ActivityIndicator
+                    animating={this.state.animating}
+                    style={[styles.centering, {height: 80}]}
+                    size="large" />
             </View>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    centering: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 8,
+    },
+    btn:{
+        marginTop:10,
+        width:150,
+        height:35,
+        backgroundColor:'#3BC1FF',
+        justifyContent:'center',
+        alignItems:'center',
+        borderRadius:4,
+    },
+});
+
+
 // 注册应用(registerComponent)后才能正确渲染
 // 注意：只把应用作为一个整体注册一次，而不是每个组件/模块都注册
-AppRegistry.registerComponent('ReactNative_View', () => ScreenReaderStatusExample);
+AppRegistry.registerComponent('ReactNative_View', () => RNActivityIndicatorDemo);
