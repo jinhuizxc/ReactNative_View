cfe46f879fd9d9722f458bd2bf00fe90b430cfd1
Your Name <1004260403@qq.com>
2017-08-17 16:17:06 +0800

    动画

--- index.android.js
+++ index.android.js
@@ -12,41 +12,53 @@ import {
     View,
     Image,
     TextInput,
+    Animated,
 } from 'react-native';
 
 /**
- * 处理文本输入
- * TextInput是一个允许用户输入文本的基础组件。它有一个名为onChangeText的属性，
- * 此属性接受一个函数，
- * 而此函数会在文本变化时被调用。另外还有一个名为onSubmitEditing的属性，
- * 会在文本被提交后（用户按下软键盘上的提交键）调用。
- * 假如我们要实现当用户输入时，实时将其以单词为单位翻译为另一种文字。
- * 我们假设这另一种文字来自某个吃货星球，只有一个单词： 🍕。
- * 所以"Hello there Bob"将会被翻译为"🍕🍕🍕"。
- * 在上面的例子里，我们把text保存到state中，因为它会随着时间变化。
+ * 动画
+ * React Native提供了两个互补的动画系统：
+ * 用于全局的布局动画LayoutAnimation，和用于创建更精细的交互控制的动画Animated。
  */
-class LotsOfStyles extends Component {
+class FadeInView extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {text: ''};
+        this.state = {
+            fadeAnim: new Animated.Value(0),          // 透明度初始值设为0
+        };
     }
-
+    componentDidMount() {
+        Animated.timing(                            // 随时间变化而执行的动画类型
+            this.state.fadeAnim,                      // 动画中的变量值
+            {
+                toValue: 1,                             // 透明度最终变为1，即完全不透明
+            }
+        ).start();                                  // 开始执行动画
+    }
     render() {
         return (
-            <View style={{padding: 10}}>
-                <TextInput
-                    style={{height: 40}}
-                    placeholder="Type here to translate!"
-                    onChangeText={(text) => this.setState({text})}/>
-                <Text style={{padding: 10, fontSize: 42}}>
-                    {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-                </Text>
-            </View>
+            <Animated.View                            // 可动画化的视图组件
+                style={{
+                    ...this.props.style,
+                    opacity: this.state.fadeAnim,          // 将透明度指定为动画变量值
+                }}
+            >
+                {this.props.children}
+            </Animated.View>
         )
     }
 }
 
+class LotsOfStyles extends Component {
+    render() {
+        return (
+            <FadeInView style={{width: 250, height: 50, backgroundColor: 'powderblue'}}>
+                <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Fading in</Text>
+            </FadeInView>
+        )
+    }
+}
 // 注册应用(registerComponent)后才能正确渲染
 // 注意：只把应用作为一个整体注册一次，而不是每个组件/模块都注册
 AppRegistry.registerComponent('ReactNative_View', () => LotsOfStyles);
